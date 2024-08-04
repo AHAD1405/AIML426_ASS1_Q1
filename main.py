3699e449f2a0d341e523a72581a9ce8fd50112d3
FAHAD <eng.bokhari@hotmail.com>
2024-08-05 00:10:09 +1200

    Prepare and obtain dataset from FILE

--- main.py
+++ main.py
@@ -5,12 +5,40 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import pandas
 
+def create_dataset(file_name):
+    weights = []
+    values = []
+    max_capacity = 0   # value of maximume weights
+    item_count = 0     # number of the items for each individual
+    optimal_value = 0  
+
+    with open(file_name,'r') as file: 
+        data = file.readlines()      
+
+        for idx, line in enumerate(data): # extract weights and vlues and store it into list
+            x = line.split()
+            if idx == 0:
+                max_capacity = int(x[1])
+                item_count = int(x[0])
+            else:
+                weights.append(int(x[1]))
+                values.append(int(x[0]))
+        
+        # Find the vlaue of optimal_value paramener. depend on value of (max_capacity) 
+        if max_capacity == 269: optimal_value = 295
+        elif max_capacity == 1000: optimal_value = 9767
+        else: optimal_value = 1514
+        
+        item_dict = {"weights":weights ,"values":values}
+
+    return item_dict, max_capacity, item_count, optimal_value
 
 def main():
     # load data 
-    data = ''
-
+    dataset_file = ''
+    items_data, max_capacity, item_count, optimal_value = create_dataset(dataset_file)  # Obtain dataset values into parameter
     # test merge brach.....
 
 if __name__ == "__main__":
